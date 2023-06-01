b4283248b1e7e69b697d8db695ab9d51d8739c0f
Mechwart András <mechwwart@gmail.com>
2023-06-01 10:07:52 +0200

    kesz

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
+using ConsoleApp1;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -12,6 +13,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            List<dij> dijak = new List<dij>();
+
+            StreamReader sr = new StreamReader("nobel.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(';');
+                dijak.Add(new dij(int.Parse(adat[0]), adat[1], adat[2], adat[3]));
+            }
+            sr.Close();
+
+            Console.WriteLine($"4. feladat: {dijak.Where(x => x.Evszam == 2017 && x.NobelTipus == "irodalmi").First().KeresztNev} {dijak.Where(x => x.Evszam == 2017 && x.NobelTipus == "irodalmi").First().VezetekNev}");
+
+            Console.WriteLine("6. feladat:");
+            dijak.Where(x => x.VezetekNev == "Curie").ToList().ForEach(x => Console.WriteLine($"{x.Evszam}: {x.KeresztNev} {x.VezetekNev}({x.NobelTipus})"));
+            Console.WriteLine("7. feladat:");
+            dijak.GroupBy(x => x.NobelTipus).ToList().ForEach(x => Console.WriteLine($"{x.Key} {x.Key.Count()}"));
         }
+
     }
 }
---
+++ ConsoleApp1/dij.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class dij
+    {
+        int evszam;
+        string nobelTipus;
+        string keresztNev;
+        string vezetekNev;
+
+        public dij(int evszam, string nobelTipus, string keresztNev, string vezetekNev)
+        {
+            this.evszam = evszam;
+            this.nobelTipus = nobelTipus;
+            this.keresztNev = keresztNev;
+            this.vezetekNev = vezetekNev;
+        }
+
+        public int Evszam { get => evszam;  }
+        public string NobelTipus { get => nobelTipus;  }
+        public string KeresztNev { get => keresztNev;  }
+        public string VezetekNev { get => vezetekNev;  }
+    }
+}
